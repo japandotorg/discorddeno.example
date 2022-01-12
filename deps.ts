eacc1a48485d2ff2b8c39ca4794c449244d1c34e
Lemon Rose <78662983+japandotorg@users.noreply.github.com>
2022-01-12 20:44:09 +0530

    deno deps

--- deps.ts
+++ deps.ts
@@ -1,2 +1,6 @@
-export * from "https://denopkg.com/discordeno/discordeno@main/mod.ts";
-export * from "https://deno.land/std@0.97.0/fmt/colors.ts";
+export * from "https://deno.land/std@0.117.0/fmt/colors.ts";
+export { config as myConfig } from "https://deno.land/x/dotenv@v3.1.0/mod.ts";
+export * from "https://deno.land/x/discordeno@13.0.0-rc15/mod.ts";
+export * from "https://deno.land/x/discordeno_helpers_plugin@0.0.3/mod.ts";
+export { validatePermissions } from "https://deno.land/x/discordeno_permissions_plugin@0.0.11/src/permissions.ts";
+export * from "https://raw.githubusercontent.com/TriForMine/kwik/4df9d84eefee7ff299cc65e0f31adb8dbee64cde/mod.ts";
